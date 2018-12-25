2dd7a7cb9dd493c3a86a246a3262ac66bab12568
Zoran Robic <vajnar7@gmail.com>
2018-12-25 08:04:14 +0100

    Implementation of GUI.

--- VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/Line.java
+++ VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/Line.java
@@ -7,7 +7,7 @@ class Line
 {
   Point p1;
   private Point p2;
-  Fun f;
+  Fun f = null;
 
   Line(Point p1, Point p2)
   {
@@ -18,21 +18,23 @@ class Line
 
   Point intersection(Line l2)
   {
-    if (f == null)
-      return null;
     float k = f.a - l2.f.a;
     float s = l2.f.c - f.c;
 
-    // linija je vertikalna ali horizontalna
-    if (l2.f.isHorizontal != null) {
-      // second line is vertical
-      if (!l2.f.isHorizontal) {
-        float yyy = f.a * l2.p1.x + f.c;
-        return new Point(l2.p1.x, yyy);
-      }
+    // second line is vertical
+    if (l2.f.isVertical) {
+      float yyy = f.a * l2.p1.x + f.c;
+      return new Point(l2.p1.x, yyy);
     }
+    // second line is horizontal
+    if (l2.f.isHorizontal) {
+      float xxx = (l2.p1.y - f.c) / f.a;
+      return new Point(xxx, l2.p1.y);
+    }
+
     if (k == 0 || s == 0)
       return null;
+
     float x = s / k;
     float y = f.a * x + f.c;
     return new Point(x, y);
@@ -43,41 +45,52 @@ class Line
     float a = p2.y - p1.y; // if 0 horizontal
     float b = p2.x - p1.x; // if 0 vertical
     if (a == 0 && b != 0)
-      f = new Fun(true);
+      f = new Fun("horizontal");
     else if (a != 0 && b == 0)
-      f = new Fun(false);
-    float m = a / b;
-    f = new Fun(m, -1, p1.y - (m * p1.x));
-  }
-
-  float f(float x)
-  {
-    float m = (p2.y - p1.y) / (p2.x - p1.x);
-    return m * (x - p1.x) + p1.y;
+      f = new Fun("vertical");
+    else if (a == 0)
+      f = new Fun("invalid");
+    else {
+      float m = a / b;
+      f = new Fun(m, -1, p1.y - (m * p1.x));
+    }
   }
 
   class Fun
   {
-    float a, b, c;
-    Boolean isHorizontal;
+    float   a            = 0;
+    float   b            = 0;
+    float   c            = 0;
+    boolean isHorizontal = false;
+    boolean isVertical   = false;
+    boolean isInvalid    = false;
 
-    Fun(boolean isH)
+    Fun(String s)
     {
-      isHorizontal = isH;
-      a = b = c = 0;
+      switch (s) {
+      case "vertical":
+        isVertical = true;
+        break;
+      case "horizontal":
+        isHorizontal = true;
+        break;
+      case "invalid":
+        isInvalid = true;
+        break;
+      }
     }
 
     Fun(float a, float b, float c)
     {
-      isHorizontal = null;
       this.a = a;
       this.b = b;
       this.c = c;
     }
   }
 
-  void draw(Canvas c, Paint p)
+  void draw(Canvas c, Paint p, int color)
   {
+    p.setColor(color);
     c.drawLine(p1.x, p1.y, p2.x, p2.y, p);
   }
 
