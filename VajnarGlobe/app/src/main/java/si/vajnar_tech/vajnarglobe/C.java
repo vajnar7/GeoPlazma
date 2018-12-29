8e8719c1e441eacb6c9a9b593ece56c10435d0db
Zoran Robic <vajnar7@gmail.com>
2018-12-29 08:47:20 +0100

    Geometry of the screen.

--- VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/C.java
+++ VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/C.java
@@ -8,5 +8,7 @@ public class C
   // DB of areas
   public static HashMap<String, Area> areas = new HashMap<>();
   // timestamper
-  public static AtomicInteger stamp = new AtomicInteger(1);
+  public static AtomicInteger         stamp = new AtomicInteger(1);
+  // screen dimensions
+  public static android.graphics.Point size = new android.graphics.Point();
 }
