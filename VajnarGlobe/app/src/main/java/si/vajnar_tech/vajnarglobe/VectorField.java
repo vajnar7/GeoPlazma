8ce90b10bd694167516561126c70cffbfd079d99
Zoran Robic <vajnar7@gmail.com>
2019-01-03 18:32:34 +0100

    Vector field. And Vector function.

--- VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/VectorField.java
+++ VajnarGlobe/app/src/main/java/si/vajnar_tech/vajnarglobe/VectorField.java
@@ -1,20 +1,16 @@
 package si.vajnar_tech.vajnarglobe;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class VectorField extends ArrayList<Vector>
 {
-  private static final int n = 7;
-  private static final AtomicInteger lim = new AtomicInteger(35);
-
   @Override
   public boolean add(Vector v)
   {
-    if (lim.getAndDecrement() > 0)
+    if (C.Parameters.lim.getAndDecrement() > 0)
       return true;
     super.add(v);
-    if (size() == n) {
+    if (size() == C.Parameters.n) {
       done(average());
       clear();
     }
@@ -26,7 +22,7 @@ public abstract class VectorField extends ArrayList<Vector>
     Vector a = new Vector();
     for (Vector v: this)
       a._plus_je(v);
-    a._deljeno_je(new Vector(n, n));
+    a._deljeno_je(new Vector(C.Parameters.n, C.Parameters.n));
     return (a);
   }
 
