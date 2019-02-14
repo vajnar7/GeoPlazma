e0ca0d5910887f144eccead04119fe1398b3daa1
Zoran Robic <vajnar7@gmail.com>
2019-02-14 20:18:28 +0100

    gitignore defining

---
+++ VajnarGlobe/vajnarlibrary/src/main/java/com/example/vajnarlibrary/VvVCalculus.java
@@ -0,0 +1,14 @@
+package com.example.vajnarlibrary;
+
+public class VvVCalculus
+{
+  //VvVFunction f;
+  public VvVCalculus()
+  {
+  }
+
+  public void rew(String exp)
+  {
+    String e = "2x^2 - 5x + 3";
+  }
+}
